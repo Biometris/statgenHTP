640da36785069870c545dd77b51544167e999acc
mille016 <emilie.millet@wur.nl>
2019-08-05 17:59:38 +0200

    data description first version

--- R/data.R
+++ R/data.R
@@ -1,36 +1,75 @@
-#' Field data for a wheat experiment in Chili.
+#' Growth chamber data for an Arabidopsis experiment in the Phenovator platform.
 #'
-#' A dataset containing field data from an experiment with wheat in Chili
-#' described in detail by Lado (2013). The experiment was performed in 2
-#' locations in Chili with 2 different drought regimes in 2011 and 2012 for
-#' the first location and 1 trial in 2012 for the second location. For 384
-#' genotypes 4 traits were measured in 2011 but in 2012 only grain yield was
-#' measured.
+#' A dataset containing growth chamber data from an experiment with Arabidopsis
+#' in the Phenovator platform (WUR, Netherlands, Flood et al. 2016). It consists of
+#' one experiment with 1440 plants grown in a growth chamber. The number of tested
+#' genotypes is 192 with 6-7 replicates per genotype. Four reference genotypes were
+#' also tested with 15 or 30 replicates. The studied trait is the photosystem II
+#' efficiency (EffpsII) extracted from the pictures over time (van Rooijen et al. 2017).
 #'
-#' @format A data.frame with 4000 rows and 11 columns:
+#' @format A data.frame with 103839 rows and 11 columns:
 #' \describe{
-#'   \item{rep}{replicate}
-#'   \item{bl}{block id}
-#'   \item{trt}{genotype}
-#'   \item{row}{row within the field}
-#'   \item{col}{column within the field}
-#'   \item{DH}{Days to Heading, the number of days from sowing till 50\% of
-#'   the spikes emerged}
-#'   \item{GY}{Grain Yield, in tons}
-#'   \item{NKS}{Number of Kernels per Spike, calculated from 25 randomly
-#'   selected spikes per plot}
-#'   \item{TKW}{Thousand Kernel Weight, in grams, calculated from 25 randomly
-#'   selected spikes per plot}
-#'   \item{trial}{trial, a combination of location and year}
-#'   \item{year}{year}
+#'   \item{Genotype}{Genotypes}
+#'   \item{Basin}{Table of experiment}
+#'   \item{Replicate}{Replicate}
+#'   \item{Image_pos}{Position of the camera}
+#'   \item{Sowing_Block}{Block defined for post-blocking}
+#'   \item{x}{row coordinate}
+#'   \item{y}{column coordinate}
+#'   \item{Sowing_Position}{unique pot ID}
+#'   \item{timepoints}{time of picture}
+#'   \item{EffpsII}{trial, a combination of location and year}
+#'   \item{pos}{unique pot using rowcol coordinate}
 #' }
 #'
-#' @source \url{http://www.g3journal.org/content/3/12/2105/}
+#' @source \url{}
 #'
-#' @references Lado, Bettina, Ivan Matus, Alejandra Rodríguez, Luis Inostroza,
-#' Jesse Poland, François Belzile, Alejandro del Pozo, Martín Quincke,
-#' Marina Castro, and Jarislav von Zitzewitz. 2013. “Increased Genomic
-#' Prediction Accuracy in Wheat Breeding Through Spatial Adjustment of Field
-#' Trial Data.” G3: Genes|Genomes|Genetics 3 (12): 2105–14.
-#' doi:10.1534/g3.113.007807.
+#' @references Rooijen, Roxanne van, Willem Kruijer, René Boesten,
+#' Fred A. van Eeuwijk, Jeremy Harbinson, and Mark G. M. Aarts. 2017.
+#' “Natural Variation of YELLOW SEEDLING1 Affects Photosynthetic Acclimation
+#' of Arabidopsis Thaliana.” Nature Communications 8 (1).
+#' doi:10.1038/s41467-017-01576-3.
+#'
+#' Flood, Pádraic J., Willem Kruijer, Sabine K. Schnabel, Rob van der Schoor,
+#' Henk Jalink, Jan F. H. Snel, Jeremy Harbinson, and Mark G. M. Aarts. 2016.
+#' “Phenomics for Photosynthesis, Growth and Reflectance in Arabidopsis Thaliana
+#' Reveals Circadian and Long-Term Fluctuations in Heritability.”
+#' Plant Methods 12 (1): 14.
+#' doi:10.1186/s13007-016-0113-y.
 "PhenovatorDat1"
+
+#' Greenhouse data for a maize experiment in the Phenoarch platform.
+#'
+#' A dataset containing greenhouse data from an experiment with maize in the
+#' Phenoarch platform (INRA, France, Cabrera-Bosquet et al. 2016). It consists of
+#' one experiment with 1671 plants grown in a greenhouse under two water scenarios,
+#' well-watered (WW) and water deficit (WD). There are two populations of genotypes,
+#' panel 1 and panel 2. Panel 1 contains 60 genotypes with 14 replicates: 7 in WW
+#' and 7 in WD. Note that thereare more plants per replicates than one (about 24
+#' plants per genotypes). Panel 2 contains 30 genotypes with 8 replicates, 4 in WW
+#' and 4 in WD. The studied trait is the leaf area extracted from the pictures over
+#' time. Plants were pictured every day for 35 days.
+#'
+#' @format A data.frame with 50405 rows and 10 columns:
+#' \describe{
+#'   \item{geno}{Genotype}
+#'   \item{population}{Panel 1 or 2}
+#'   \item{Scenario}{Water regime}
+#'   \item{Rep}{Replicate}
+#'   \item{Line}{line of conveyor belt (i.e column coordinate)}
+#'   \item{Position}{pot position on the conveyor belt (i.e. row coordinate)}
+#'   \item{Pot}{unique pot ID}
+#'   \item{LA_Estimated}{Leaf area from the picture (m^2^)}
+#'   \item{Date}{Date of measurement}
+#'   \item{pos}{unique pot using rowcol coordinate}
+#' }
+#'
+#' @source \url{}
+#'
+#' @references Cabrera-Bosquet, Llorenç, Fournier Christian, Brichet Nicolas,
+#' Welcker Claude, Suard Benoît, and Tardieu François. 2016.
+#' “High-throughput estimation of incident light, light interception and
+#' radiation-use efficiency of thousands of plants in a phenotyping platform.”
+#' New Phytologist 212 (1): 269-81.
+#' doi:10.1111/nph.14027.
+"PhenoarchDat1"
