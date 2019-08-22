4d652801fd5845366a522c2aca4a3cef205d5521
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2019-08-22 10:03:45 +0200

    Added option trait to fitSpline

--- R/fitSpline.R
+++ R/fitSpline.R
@@ -3,29 +3,34 @@
 #' Function for fitting splines.
 #'
 #' @param corrDat A data.frame with corrected spatial data.
+#' @param trait A character string indicating the trait for which the spline
+#' should be fitted.
 #' @param n Just here for testing
 #' @param knots The number of knots to use when fitting the spline.
 #'
 #' @export
 fitSpline <- function(corrDat,
+                      trait,
                       n = nlevels(corrDat[["plotId"]]),
                       knots = 50) {
   ## Create data.frame with plants and genotypes for adding genotype to results.
   plantGeno <- unique(corrDat[c("plotId", "genotype")])
   ## determine minimum number of time points requiered.
   minTP <- 0.8 * length(unique(corrDat[["timeNumber"]]))
+  ## Construct formula for fitting model.
+  modForm <- as.formula(paste(trait, "~s(timeNumber, bs = 'ps', k = ",
+                              knots, ")"))
   ## Fit splines.
   fitSp <- lapply(X = levels(corrDat[["plotId"]])[1:n], FUN = function(plant) {
     ## Restrict data to current plant.
-    dat <- corrDat[corrDat[["plotId"]] == plant, c("timeNumber", "EffpsII_corr")]
+    dat <- corrDat[corrDat[["plotId"]] == plant, c("timeNumber", trait)]
     dat <- droplevels(dat)
     ## Manually select minimum number of time points.
     if (length(unique(dat[["timeNumber"]])) >= minTP) {
       ## Fit the P-spline using gam() function in mgcv.
       ## Manually set the number of knots
       ## Depends on the nb of time points and shape of the curve)
-      obj <- mgcv::gam(EffpsII_corr ~ s(timeNumber, bs = "ps", k = knots),
-                       data = dat, method = "REML")
+      obj <- mgcv::gam(modForm, data = dat, method = "REML")
       ## Extract the spline coefficients.
       coeff <- data.frame(obj$coefficients, plotId = plant)
       ## Predictions on a dense grid.
