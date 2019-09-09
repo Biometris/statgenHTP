59aa291eca0d00642e959a85b936f88a30e6a026
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2019-09-09 12:30:57 +0200

    Added test for prettier

--- inst/tinytest/test_utils.R
+++ inst/tinytest/test_utils.R
@@ -50,4 +50,51 @@ expect_equal(statgenHTP:::dfBind(list(data.frame(), df1)), df1)
 expect_equal(statgenHTP:::dfBind(list(df1, data.frame())), df1)
 expect_equal(statgenHTP:::dfBind(list(data.frame())), data.frame())
 
+### Test addMissVals
+
+times <- strptime(c("1sep2019", "2sep2019"), "%d%b%Y")
+timeNums <- c(1, 1, 2)
+df1 <- data.frame(timePoint = times[timeNums], timeNumber = timeNums,
+                  id = c("p1", "p2", "p1"), trait = 1:3)
+
+## Check that single missing value is added when no extra columns present.
+dfOut1 <- statgenHTP:::addMissVals(df1, "trait")
+expect_true(inherits(dfOut1, "data.frame"))
+expect_true(inherits(dfOut1[["timePoint"]], "POSIXct"))
+expect_equal(nrow(dfOut1), 4)
+expect_equivalent(unlist(dfOut1[4, ]), c(2, 1567382400, NA))
+
+## Check that multiple missing values are added when no extra columns present.
+df2 <- df1[-1, ]
+dfOut2 <- statgenHTP:::addMissVals(df2, "trait")
+expect_equal(nrow(dfOut2), 4)
+expect_equivalent(unlist(dfOut2[1, ]), c(1, 1567296000, NA))
+expect_equivalent(unlist(dfOut2[4, ]), c(2, 1567382400, NA))
+
+## Check that single missing value is added when extra columns present.
+df3 <- df1
+df3[["treat"]] <- c("W", "D", "W")
+dfOut3 <- statgenHTP:::addMissVals(df3, "trait")
+expect_equal(dfOut3[colnames(dfOut3) != "treat"], dfOut1)
+expect_equal(dfOut3[["treat"]], c("W", "D", "W" , "D"))
+
+## Check that multiple missing values are added when extra columns present.
+df4 <- df2
+df4[["treat"]] <- c("D", "W")
+dfOut4 <- statgenHTP:::addMissVals(df4, "trait")
+expect_equal(dfOut4[colnames(dfOut4) != "treat"], dfOut2)
+expect_equal(dfOut4[["treat"]], c("W", "D", "W" , "D"))
+
+### Test prettier
+
+## Different splits for n = 1, 2 and 3.
+## Use 40 day time difference for ease of checking.
+times <- strptime(c("1sep2019", "11oct2019"), "%d%b%Y")
+expect_equal(statgenHTP:::prettier(n = 1)(times),
+             strptime("21sep2019", "%d%b%Y"))
+expect_equal(statgenHTP:::prettier(n = 2)(times),
+             strptime(c("9sep2019", "3oct2019"), "%d%b%Y"))
+expect_equal(statgenHTP:::prettier(n = 3)(times),
+             strptime(c("6sep2019", "21sep2019", "6oct2019"), "%d%b%Y"))
+
 
