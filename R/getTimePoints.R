7f3fc52444b710e28a5655a5f8127df479095dc7
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2019-11-18 11:55:27 +0100

    Moved getTimePoints to separate file

---
+++ R/getTimePoints.R
@@ -0,0 +1,34 @@
+#' Extract time points
+#'
+#' Function for extracting a data.frame with timeNumbers and timePoints from
+#' an object of class TP or fitMod.
+#'
+#' @param x An object of class TP or fitMod
+#'
+#' @return A data.frame with columns timeNumber and timePoint listing the
+#' time points in x
+#'
+#' @examples
+#' ## Create an object of class TP
+#' data("PhenovatorDat1")
+#' phenoTP <- createTimePoints(dat = PhenovatorDat1,
+#'                             experimentName = "Phenovator",
+#'                             genotype = "Genotype",
+#'                             timePoint = "timepoints",
+#'                             repId = "Replicate",
+#'                             plotId = "pos",
+#'                             rowNum = "y", colNum = "x",
+#'                             addCheck = TRUE,
+#'                             checkGenotypes = c("check1", "check2", "check3", "check4"))
+#'
+#' ## Extract the time points from the object.
+#' head(getTimePoints(phenoTP))
+#'
+#' @export
+getTimePoints <- function(x) {
+  ## Check input.
+  if (!inherits(x, "TP") && !inherits(x, "fitMod")) {
+    stop("x should be an object of class TP or fitMod")
+  }
+  return(attr(x = x, which = "timePoints"))
+}
