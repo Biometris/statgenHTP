eea0273bc8807f51de06b298e9bf1309a2fdca4c
mille016 <emilie.millet@wur.nl>
2020-07-29 18:48:35 +0200

    modify datasets and start splitting the vignette

--- data-raw/exampleData.R
+++ data-raw/exampleData.R
@@ -10,19 +10,28 @@ PhenovatorDat1$pos <- paste0("c", PhenovatorDat1[["x"]],
                              "r", PhenovatorDat1[["y"]])
 # Remove a plant that has very few measurements.
 PhenovatorDat1 <- PhenovatorDat1[PhenovatorDat1$pos != "c1r54",]
-
 # Export to package
 usethis::use_data(PhenovatorDat1, overwrite = TRUE)
 
 
 # Read raw data.
-PhenoarchDat1 <- read.csv(system.file("extdata", "Phenoarch_Data_ZA17.csv",
+# PhenoarchDat1 <- read.csv(system.file("extdata", "Phenoarch_Data_ZA17.csv",
+#                                       package = "statgenHTP"))
+PhenoarchDat1 <- read.csv(system.file("extdata", "Phenoarch_ZA17_extraVariables.csv",
                                       package = "statgenHTP"))
 # Create an indicator for each plot (according to the row and column position)
-PhenoarchDat1$pos <- paste0("c", PhenoarchDat1$Line, "r", PhenoarchDat1$Position)
-
+# PhenoarchDat1$pos <- paste0("c", PhenoarchDat1$Line, "r", PhenoarchDat1$Position)
 # Export to package
 usethis::use_data(PhenoarchDat1, overwrite = TRUE)
+
+
+# Read raw data.
+RootDat1 <- read.csv(system.file("extdata", "Data_tipclean_format.csv",
+                                      package = "statgenHTP"))
+# select one tank
+RootDat1 <- RootDat1[RootDat1$Tank == "A",]
+# Export to package
+usethis::use_data(RootDat1, overwrite = TRUE)
 
 
 ## Create data for testing.
