c52057ad1d2a90e55920f661d34f049ce6fdbd43
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2020-08-11 08:38:09 +0200

    Added function as.data.frame.TP

--- R/createTimePoints.R
+++ R/createTimePoints.R
@@ -756,5 +756,15 @@ plot.TP <- function(x,
 }
 
 
+#' Function for converting an object of class TP to a data.frame.
+#'
+#' @param x An object of class TP.
+#'
+#' @export
+as.data.frame.TP <- function (x,
+                              ...) {
+  res <- dfBind(x)
+  return(res)
+}
 
 
