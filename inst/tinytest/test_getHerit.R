5aa6c27cb60d7ecea1b6502993204b034532a706
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2020-12-23 09:48:21 +0100

    Fixed getHerit tests (#71)

--- inst/tinytest/test_getHerit.R
+++ inst/tinytest/test_getHerit.R
@@ -1,4 +1,4 @@
-### Test getCorrected.
+### Test getHerit.
 
 ## Read test data from .csv
 testDat <- read.csv("testDat.csv", stringsAsFactors = FALSE)
@@ -23,8 +23,6 @@ expect_error(getHerit("fitMod"),
 herit1 <- getHerit(testFitMod1)
 herit2 <- getHerit(testFitMod2)
 
-if (FALSE) {
-
 ## Check output structure.
 
 expect_true(inherits(herit1, "data.frame"))
@@ -36,9 +34,9 @@ expect_equal(dim(herit2), c(5, 4))
 expect_equal(colnames(herit2), c("timeNumber", "timePoint", "1", "2"))
 
 # Compare results.
-expect_equal(herit1[["h2"]], c(0, 0.21, 0.01, 0.91, 0.42))
-expect_equal(herit2[["1"]], c(0, 0.7, 0.02, 0.49, 0))
-expect_equal(herit2[["2"]], c(0.3, 0.77, 0.58, 0.78, 0.71))
+expect_equal(herit1[["h2"]], c(0, 0.22, 0.03, 0.88, 0.44))
+expect_equal(herit2[["1"]], c(0, 0.69, 0.02, 0.45, 0))
+expect_equal(herit2[["2"]], c(0.3, 0.78, 0.59, 0.78, 0.73))
 
 ## Check that results can be written to a file.
 tmpFile <- tempfile(fileext = ".csv")
@@ -80,9 +78,4 @@ if (at_home()) {
                c(-1.9124297023998e-05, 0.951117118858154, 0.941710391410272,
                  0.767914363565008, 0.646403039223904))
 }
-
-
-
-}
-
 
