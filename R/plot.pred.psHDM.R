dc503166071fb56c51cd80942abca6edb695dce0
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2022-06-08 12:14:03 +0200

    Removed missing values warnings

--- R/plot.pred.psHDM.R
+++ R/plot.pred.psHDM.R
@@ -74,128 +74,183 @@ plot.psHDM <- function(object,
                                           geno.dev = NULL,
                                           plant.tra = NULL)){
 
-  if(!inherits(object, "psHDM")) {
+  if (!inherits(object, "psHDM")) {
     stop("The object class is not correct")
   }
-
-  if(is.null(xlab)){
-    xlab = "Time"
+  if (is.null(xlab)) {
+    xlab <- "Time"
   }
-
-  if(is.null(ylab)){
-    ylab = expression(tilde(y)[pgi](t))
+  if (is.null(ylab)) {
+    ylab <- expression(tilde(y)[pgi](t))
   }
-
   if(is.null(object$plant.obs)){
     object$plant.obs <- object$plant.level
   }
-  # object as data frames
-  # object <- list.to.df(object = object)
-
   ntime <- length(unique(object$plant.obs$timeNumber))
   min.t <- min(object$plant.obs$timeNumber)
   max.t <- max(object$plant.obs$timeNumber)
-
-  if(!is.null(object$pop.level)){
-    # Population-specific growth curves
+  if (!is.null(object$pop.level)) {
+    ## Population-specific growth curves.
     my.cols <- c("1" = "gray", "2" = "blue")
     aa1 <- ggplot2::ggplot(data = object$plant.obs) +
-      ggplot2::geom_line(ggplot2::aes(timeNumber, obs_plant, group = plant, colour = "1")) +
-      # ggplot2::geom_line(data = object$plant.level[!is.na(object$plant.level$obs_plant),],
-      #                    ggplot2::aes(timeNumber, obs_plant, group = plant, colour = "1")) +
-      # ggplot2::geom_point(ggplot2::aes(timeNumber, obs_plant, group = plant, colour = "1"), shape = 20) +
-      ggplot2::geom_line(data = object$pop.level, ggplot2::aes(timeNumber, f_pop, group = pop, colour="2")) +
-      ggplot2::geom_rug(ggplot2::aes(x = timeNumber, y = NULL), color = "gray", length = ggplot2::unit(0.01, "npc")) +
+      ggplot2::geom_line(ggplot2::aes(timeNumber, obs_plant, group = plant,
+                                      colour = "1"), na.rm = TRUE) +
+      ggplot2::geom_line(data = object$pop.level,
+                         ggplot2::aes(timeNumber, f_pop, group = pop,
+                                      colour = "2"), na.rm = TRUE) +
+      ggplot2::geom_rug(ggplot2::aes(x = timeNumber, y = NULL), color = "gray",
+                        length = ggplot2::unit(0.01, "npc")) +
       ggplot2::scale_x_continuous(breaks = round(seq(min.t, max.t, length.out = 5), 0)) +
-      ggplot2::scale_color_manual(values = my.cols, labels = c(expression(tilde(y)[pgi](t)),expression(hat(f)[p](t))))+
+      ggplot2::scale_color_manual(values = my.cols,
+                                  labels = c(expression(tilde(y)[pgi](t)),
+                                             expression(hat(f)[p](t)))) +
       ggplot2::labs(x = xlab, y = ylab, title = global.main$pop.tra, color = "") +
-      my.theme() + ggplot2::facet_grid( ~ pop)
-    if(!is.null(object$pop.level$se_pop)){
-      aa1 <- aa1 + ggplot2::geom_ribbon(data = object$pop.level, ggplot2::aes(x = timeNumber, ymin = f_pop - 1.96 * se_pop, ymax = f_pop + 1.96 * se_pop, group = pop), fill = ggplot2::alpha("blue", 0.3))
+      my.theme() +
+      ggplot2::facet_grid( ~ pop)
+    if (!is.null(object$pop.level$se_pop)) {
+      aa1 <- aa1 +
+        ggplot2::geom_ribbon(data = object$pop.level,
+                             ggplot2::aes(x = timeNumber,
+                                          ymin = f_pop - 1.96 * se_pop,
+                                          ymax = f_pop + 1.96 * se_pop,
+                                          group = pop),
+                             fill = ggplot2::alpha("blue", 0.3))
     }
     print(aa1)
   }
-
-  if(!is.null(object$geno.level)){
-    # Genotype-specific growth curves
+  if (!is.null(object$geno.level)) {
+    ## Genotype-specific growth curves.
     my.cols <- c("1" = "blue", "2" = "orange")
     bb1 <- ggplot2::ggplot(data = object$geno.level) +
-      ggplot2::geom_line(ggplot2::aes(timeNumber, f_geno, group = geno, colour="1")) +
-      ggplot2::geom_line(data = object$pop.level, ggplot2::aes(timeNumber, f_pop, group = pop, colour="2"), size = 0.8) +
-      ggplot2::geom_rug(data = object$plant.obs, ggplot2::aes(x = timeNumber, y = NULL), color = "gray", length = ggplot2::unit(0.01, "npc")) +
+      ggplot2::geom_line(ggplot2::aes(timeNumber, f_geno, group = geno,
+                                      colour = "1"), na.rm = TRUE) +
+      ggplot2::geom_line(data = object$pop.level,
+                         ggplot2::aes(timeNumber, f_pop, group = pop,
+                                      colour="2"), size = 0.8, na.rm = TRUE) +
+      ggplot2::geom_rug(data = object$plant.obs,
+                        ggplot2::aes(x = timeNumber, y = NULL), color = "gray",
+                        length = ggplot2::unit(0.01, "npc")) +
       ggplot2::scale_x_continuous(breaks = round(seq(min.t, max.t, length.out = 5), 0)) +
-      ggplot2::scale_color_manual(values = my.cols, labels = c(expression(hat(f)[p](t)+hat(f)[pg](t)),expression(hat(f)[p](t))))+
-      ggplot2::labs(x = xlab, y = ylab, color = "", title = global.main$geno.tra) +
-      my.theme() + ggplot2::facet_grid( ~ pop)
-    if(!is.null(object$pop.level$se_pop)){
-      bb1 <- bb1 + ggplot2::geom_ribbon(data = object$pop.level, ggplot2::aes(x = timeNumber, ymin = f_pop - 1.96 * se_pop, ymax = f_pop + 1.96 * se_pop, group = pop), fill = ggplot2::alpha("orange", 0.5))
+      ggplot2::scale_color_manual(values = my.cols,
+                                  labels = c(expression(hat(f)[p](t)+hat(f)[pg](t)),
+                                             expression(hat(f)[p](t)))) +
+      ggplot2::labs(x = xlab, y = ylab, color = "",
+                    title = global.main$geno.tra) +
+      my.theme() +
+      ggplot2::facet_grid( ~ pop)
+    if (!is.null(object$pop.level$se_pop)) {
+      bb1 <- bb1 +
+        ggplot2::geom_ribbon(data = object$pop.level,
+                             ggplot2::aes(x = timeNumber,
+                                          ymin = f_pop - 1.96 * se_pop,
+                                          ymax = f_pop + 1.96 * se_pop,
+                                          group = pop),
+                             fill = ggplot2::alpha("orange", 0.5))
     }
-    if(ask) readline("Press return for next page....")
+    if (ask) readline("Press return for next page....")
     print(bb1)
-
-    # First derivative of the genotype-specific growth curves
-    my.cols <- c("1" = "blue", "2" = "orange")
+    ## First derivative of the genotype-specific growth curves.
     bb2 <- ggplot2::ggplot(data = object$geno.level) +
-      ggplot2::geom_line(ggplot2::aes(timeNumber, f_geno_deriv1, group = geno, colour = "1")) +
-      ggplot2::geom_line(data = object$pop.level, ggplot2::aes(timeNumber, f_pop_deriv1, group = pop, colour = "2")) +
-      ggplot2::geom_rug(data = object$plant.obs, ggplot2::aes(x = timeNumber, y = NULL), color = "gray", length = ggplot2::unit(0.01, "npc")) +
+      ggplot2::geom_line(ggplot2::aes(timeNumber, f_geno_deriv1, group = geno,
+                                      colour = "1"), na.rm = TRUE) +
+      ggplot2::geom_line(data = object$pop.level,
+                         ggplot2::aes(timeNumber, f_pop_deriv1, group = pop,
+                                      colour = "2"), na.rm = TRUE) +
+      ggplot2::geom_rug(data = object$plant.obs,
+                        ggplot2::aes(x = timeNumber, y = NULL), color = "gray",
+                        length = ggplot2::unit(0.01, "npc")) +
       ggplot2::scale_x_continuous(breaks = round(seq(min.t, max.t, length.out = 5), 0)) +
-      ggplot2::ylim(min(object$geno.level$f_geno_deriv1), max(object$geno.level$f_geno_deriv1)*1.2) +
-      ggplot2::scale_color_manual(values = my.cols, labels = c(expression((hat(f)[p](t)+hat(f)[pg](t))*minute),expression((hat(f)[p](t))*minute)))+
-      ggplot2::labs(x = xlab, y = "First derivative", color = "", title = global.main$geno.tra.deriv1) +
-      my.theme() + ggplot2::facet_grid( ~ pop)
-    if(!is.null(object$pop.level$se_pop_deriv1)){
-      bb2 <- bb2 + ggplot2::geom_ribbon(data = object$pop.level, ggplot2::aes(x = timeNumber, ymin = f_pop_deriv1 - 1.96 * se_pop_deriv1, ymax = f_pop_deriv1 + 1.96 * se_pop_deriv1, group = pop), fill = ggplot2::alpha("orange", 0.5))
+      ggplot2::ylim(min(object$geno.level$f_geno_deriv1),
+                    max(object$geno.level$f_geno_deriv1) * 1.2) +
+      ggplot2::scale_color_manual(values = my.cols,
+                                  labels = c(expression((hat(f)[p](t)+hat(f)[pg](t))*minute),
+                                             expression((hat(f)[p](t))*minute))) +
+      ggplot2::labs(x = xlab, y = "First derivative", color = "",
+                    title = global.main$geno.tra.deriv1) +
+      my.theme() +
+      ggplot2::facet_grid( ~ pop)
+    if (!is.null(object$pop.level$se_pop_deriv1)) {
+      bb2 <- bb2 +
+        ggplot2::geom_ribbon(data = object$pop.level,
+                             ggplot2::aes(x = timeNumber,
+                                          ymin = f_pop_deriv1 - 1.96 * se_pop_deriv1,
+                                          ymax = f_pop_deriv1 + 1.96 * se_pop_deriv1,
+                                          group = pop),
+                             fill = ggplot2::alpha("orange", 0.5))
     }
     if(ask) readline("Press return for next page....")
     print(bb2)
-
-    # Estimated genotypic deviations
+    ## Estimated genotypic deviations.
     my.cols <- c("1" = "blue")
     cc1 <- ggplot2::ggplot(data = object$geno.level) +
-      ggplot2::geom_line(ggplot2::aes(timeNumber, f_geno_dev, group = geno, colour="1")) +
-      ggplot2::geom_rug(data = object$plant.obs, ggplot2::aes(x = timeNumber, y = NULL), color = "gray", length = ggplot2::unit(0.01, "npc")) +
+      ggplot2::geom_line(ggplot2::aes(timeNumber, f_geno_dev, group = geno,
+                                      colour = "1"), na.rm = TRUE) +
+      ggplot2::geom_rug(data = object$plant.obs,
+                        ggplot2::aes(x = timeNumber, y = NULL), color = "gray",
+                        length = ggplot2::unit(0.01, "npc")) +
       ggplot2::scale_x_continuous(breaks = round(seq(min.t, max.t, length.out = 5), 0)) +
-      ggplot2::scale_color_manual(values = my.cols, labels = c(expression(hat(f)[pg](t))))+
-      ggplot2::labs(x = xlab, y = ylab, title = global.main$geno.dev ,color = "") +
-      my.theme() + ggplot2::facet_grid( ~ pop)
+      ggplot2::scale_color_manual(values = my.cols,
+                                  labels = c(expression(hat(f)[pg](t)))) +
+      ggplot2::labs(x = xlab, y = ylab, title = global.main$geno.dev,
+                    color = "") +
+      my.theme() +
+      ggplot2::facet_grid( ~ pop)
     if(ask) readline("Press return for next page....")
     print(cc1)
   }
-
-  # Estimated plant trajectories
-  if(!is.null(object$plant.level)) {
-    df.tra.HDM.sub <- droplevels(object$plant.level[object$plant.level$geno %in% geno.sub,])
-    df.obs.HDM.sub <- droplevels(object$plant.obs[object$plant.obs$geno %in% geno.sub,])
-    df.tra.HDM.geno.sub <- droplevels(object$geno.level[object$geno.level$geno %in% geno.sub,])
-
-    if(!is.null(geno.sub.names)){
-      df.tra.HDM.sub$geno      <- factor(df.tra.HDM.sub$geno[drop=TRUE], labels = geno.sub.names)
-      df.obs.HDM.sub$geno      <- factor(df.obs.HDM.sub$geno[drop=TRUE], labels = geno.sub.names)
-      df.tra.HDM.geno.sub$geno <- factor(df.tra.HDM.geno.sub$geno[drop=TRUE], labels = geno.sub.names)
+  ## Estimated plant trajectories.
+  if (!is.null(object$plant.level)) {
+    df.tra.HDM.sub <- droplevels(object$plant.level[object$plant.level$geno %in% geno.sub, ])
+    df.obs.HDM.sub <- droplevels(object$plant.obs[object$plant.obs$geno %in% geno.sub, ])
+    df.tra.HDM.geno.sub <- droplevels(object$geno.level[object$geno.level$geno %in% geno.sub, ])
+    if (!is.null(geno.sub.names)) {
+      df.tra.HDM.sub$geno      <- factor(df.tra.HDM.sub$geno[drop = TRUE],
+                                         labels = geno.sub.names)
+      df.obs.HDM.sub$geno      <- factor(df.obs.HDM.sub$geno[drop = TRUE],
+                                         labels = geno.sub.names)
+      df.tra.HDM.geno.sub$geno <- factor(df.tra.HDM.geno.sub$geno[drop = TRUE],
+                                         labels = geno.sub.names)
     }
     if(!is.null(geno.sub.order)){
-      df.tra.HDM.sub$geno      <- factor(df.tra.HDM.sub$geno, levels = levels(df.tra.HDM.sub$geno)[geno.sub.order])
-      df.obs.HDM.sub$geno      <- factor(df.obs.HDM.sub$geno, levels = levels(df.obs.HDM.sub$geno)[geno.sub.order])
-      df.tra.HDM.geno.sub$geno <- factor(df.tra.HDM.geno.sub$geno, levels = levels(df.tra.HDM.geno.sub$geno)[geno.sub.order])
+      df.tra.HDM.sub$geno      <- factor(df.tra.HDM.sub$geno,
+                                         levels = levels(df.tra.HDM.sub$geno)[geno.sub.order])
+      df.obs.HDM.sub$geno      <- factor(df.obs.HDM.sub$geno,
+                                         levels = levels(df.obs.HDM.sub$geno)[geno.sub.order])
+      df.tra.HDM.geno.sub$geno <- factor(df.tra.HDM.geno.sub$geno,
+                                         levels = levels(df.tra.HDM.geno.sub$geno)[geno.sub.order])
     }
-
     my.cols <- c("1" = "gray", "2" = "blue", "3" = "red")
     dd1 <- ggplot2::ggplot(data = df.obs.HDM.sub) +
-      ggplot2::geom_line(ggplot2::aes(timeNumber, obs_plant, group = plant, colour = "1")) +
-      # ggplot2::geom_line(data = df.obs.HDM.sub[!is.na(df.obs.HDM.sub$obs_plant),], ggplot2::aes(timeNumber, obs_plant, group = plant, colour = "1")) +
-      # ggplot2::geom_point(ggplot2::aes(timeNumber, obs_plant, group = plant, colour = "1"), shape = 20) +
-      ggplot2::geom_line(data = df.tra.HDM.sub, ggplot2::aes(timeNumber, f_plant, group = plant, colour = "2"), linetype = 2) +
-      ggplot2::geom_line(data = df.tra.HDM.geno.sub, ggplot2::aes(timeNumber, f_geno, group = geno, colour = "3")) +
-      ggplot2::geom_rug(data = df.obs.HDM.sub, ggplot2::aes(x = timeNumber, y = NULL), color = "gray", length = ggplot2::unit(0.01, "npc")) +
+      ggplot2::geom_line(ggplot2::aes(timeNumber, obs_plant, group = plant,
+                                      colour = "1"), na.rm = TRUE) +
+      ggplot2::geom_line(data = df.tra.HDM.sub,
+                         ggplot2::aes(timeNumber, f_plant, group = plant,
+                                      colour = "2"), linetype = 2, na.rm = TRUE) +
+      ggplot2::geom_line(data = df.tra.HDM.geno.sub,
+                         ggplot2::aes(timeNumber, f_geno, group = geno,
+                                      colour = "3"), na.rm = TRUE) +
+      ggplot2::geom_rug(data = df.obs.HDM.sub,
+                        ggplot2::aes(x = timeNumber, y = NULL),
+                        color = "gray", length = ggplot2::unit(0.01, "npc")) +
       ggplot2::scale_x_continuous(breaks = round(seq(min.t, max.t, length.out = 5), 0)) +
-      # scale_y_continuous(breaks = round(seq(min(df.obs.HDM.sub$obs_plant, na.rm = TRUE), max(df.obs.HDM.sub$obs_plant, na.rm = TRUE)*1.5, length.out = 5), 2)) +
-      ggplot2::ylim(min(df.obs.HDM.sub$obs_plant, na.rm = TRUE), max(df.obs.HDM.sub$obs_plant, na.rm = TRUE)*1.2) +
-      ggplot2::scale_color_manual(values = my.cols, labels = c(expression(tilde(y)[pgi](t)),expression(hat(f)[p](t)+hat(f)[pg](t)+hat(f)[pgi](t)),expression(hat(f)[p](t)+hat(f)[pg](t))))+
-      ggplot2::labs(x = xlab, y = ylab, title = global.main$plant.tra, color = "") +
-      my.theme() + ggplot2::facet_grid( ~ geno)
-    if(!is.null(object$geno.level$se_geno)){
-      dd1 <- dd1 + ggplot2::geom_ribbon(data = df.tra.HDM.geno.sub, ggplot2::aes(x = timeNumber, ymin = f_geno - 1.96 * se_geno, ymax = f_geno + 1.96 * se_geno, group = pop), fill = ggplot2::alpha("orange", 0.5))
+      ggplot2::ylim(min(df.obs.HDM.sub$obs_plant, na.rm = TRUE),
+                    max(df.obs.HDM.sub$obs_plant, na.rm = TRUE) * 1.2) +
+      ggplot2::scale_color_manual(values = my.cols,
+                                  labels = c(expression(tilde(y)[pgi](t)),
+                                             expression(hat(f)[p](t)+hat(f)[pg](t)+hat(f)[pgi](t)),
+                                             expression(hat(f)[p](t)+hat(f)[pg](t)))) +
+      ggplot2::labs(x = xlab, y = ylab, title = global.main$plant.tra,
+                    color = "") +
+      my.theme() +
+      ggplot2::facet_grid( ~ geno)
+    if (!is.null(object$geno.level$se_geno)) {
+      dd1 <- dd1 +
+        ggplot2::geom_ribbon(data = df.tra.HDM.geno.sub,
+                             ggplot2::aes(x = timeNumber,
+                                          ymin = f_geno - 1.96 * se_geno,
+                                          ymax = f_geno + 1.96 * se_geno,
+                                          group = pop),
+                             fill = ggplot2::alpha("orange", 0.5))
     }
     if(ask) readline("Press return for next page....")
     print(dd1)
@@ -213,14 +268,17 @@ plot.psHDM <- function(object,
 my.theme <- function(my.size = 20) {
   ggplot2::theme(strip.text.x = ggplot2::element_text(size = my.size + 5),
                  strip.text.y = ggplot2::element_text(size = my.size + 5),
-                 plot.title = ggplot2::element_text(hjust = 0.5, size = my.size + 5, face = "bold"),
+                 plot.title = ggplot2::element_text(hjust = 0.5,
+                                                    size = my.size + 5,
+                                                    face = "bold"),
                  axis.text = ggplot2::element_text(size = my.size),
                  axis.title = ggplot2::element_text(size = my.size + 5),
                  legend.title = ggplot2::element_text(size = my.size),
-                 legend.text = ggplot2::element_text(size = my.size + 5, hjust = 0),
+                 legend.text = ggplot2::element_text(size = my.size + 5,
+                                                     hjust = 0),
                  legend.background = ggplot2::element_rect(fill = ggplot2::alpha("white", 0)),
                  legend.key = ggplot2::element_rect(fill = ggplot2::alpha("white", 0)),
-                 legend.justification = c(1,1),
+                 legend.justification = c(1, 1),
                  legend.position = "top",
                  panel.background = ggplot2::element_rect(fill = "gray95"))
 }
