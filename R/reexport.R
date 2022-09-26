a0d894ced2810a53923834bc5873a0e8a41e971b
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2022-09-26 11:53:14 +0200

    Updated vignette

---
+++ R/reexport.R
@@ -0,0 +1,6 @@
+#' @keywords internal
+dlogdet <- utils::getFromNamespace(x = "dlogdet", ns = "LMMsolver")
+
+#' @keywords internal
+ADchol <- utils::getFromNamespace(x = "ADchol", ns = "LMMsolver")
+
